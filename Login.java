ddc83a0886ba6925d04b2d0156a76150341b3789
Rachel Potter <darksoulkit@gmail.com>
2022-04-20 12:11:56 -0400

    aesthetics

--- Login.java
+++ Login.java
@@ -14,18 +14,20 @@ public class Login extends JFrame {
         JFrame frame = new JFrame("Login");
         JPanel panel;
         GridBagConstraints c;
-        JLabel label1 = new JLabel("Wordle");
+        JLabel label1 = new JLabel("WORDLE");
         JLabel label2 = new JLabel("Please enter your username:");
         JTextField jTextField1 = new JTextField(20);
-        JButton jButton1 = new JButton("Enter");
+        JButton jButton1 = new JButton("ENTER");
 
         panel = new JPanel();
         panel.setLayout(new GridBagLayout());
+        panel.setBackground(new Color(18,18,19));
 
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setSize(260, 200);
 
         label1.setFont(new Font("Arial", Font.BOLD, 36));
+        label1.setForeground(new Color(255, 255, 255));
         label1.setAlignmentX(0.5F);
         c = new GridBagConstraints();
         c.gridx = 0;
@@ -34,6 +36,7 @@ public class Login extends JFrame {
         panel.add(label1, c);
 
         label2.setFont(new Font("Arial", Font.PLAIN, 14));
+        label2.setForeground(new Color(255, 255, 255));
         label2.setAlignmentX(0.5F);
         c = new GridBagConstraints();
         c.gridx = 0;
@@ -43,6 +46,7 @@ public class Login extends JFrame {
 
         jTextField1.setFont(new Font("Arial", Font.BOLD, 14));
         jTextField1.setSize(200, 18);
+        jTextField1.setBorder(BorderFactory.createEmptyBorder());
         jTextField1.setAlignmentX(0.5F);
         c = new GridBagConstraints();
         c.gridx = 0;
@@ -50,9 +54,10 @@ public class Login extends JFrame {
         c.insets = new Insets(6, 6, 6, 6);
         panel.add(jTextField1, c);
 
-        jButton1.setFont(new Font("Arial", Font.PLAIN, 14));
         jButton1.setFont(new Font("Arial", Font.BOLD, 20));
+        jButton1.setBackground(new Color(83, 141, 78));
         jButton1.setBorderPainted(false);
+        jButton1.setForeground(new Color(255, 255, 255));
         jButton1.setPreferredSize(new Dimension(151, 52));
         c = new GridBagConstraints();
         c.gridx = 0;
--- displayStats.java
+++ displayStats.java
@@ -46,6 +46,7 @@ public class displayStats extends JFrame {
         frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
 
         panel = new JPanel();
+        panel.setBackground(new Color(18, 18, 19));
         panel.setPreferredSize(new Dimension(120, 80));
         GridBagLayout panelLayout = new GridBagLayout();
         panelLayout.columnWidths = new int[]{60};
@@ -53,6 +54,7 @@ public class displayStats extends JFrame {
 
         label1.setFont(new Font("Arial", Font.BOLD, 24));
         label1.setText("STATISTICS");
+        label1.setForeground(new Color(255, 255, 255));
         label1.setHorizontalAlignment(SwingConstants.CENTER);
         c = new GridBagConstraints();
         c.gridx = 0;
@@ -62,8 +64,9 @@ public class displayStats extends JFrame {
         c.anchor = GridBagConstraints.PAGE_START;
         panel.add(label1, c);
 
-        label2.setFont(new Font("Dialog", Font.PLAIN, 36));
+        label2.setFont(new Font("Arial", Font.PLAIN, 36));
         label2.setText(String.valueOf(stats.getGamesPlayed())); //played
+        label2.setForeground(new Color(255, 255, 255));
         label2.setHorizontalAlignment(SwingConstants.CENTER);
         c = new GridBagConstraints();
         c.gridx = 0;
@@ -71,8 +74,9 @@ public class displayStats extends JFrame {
         c.fill = GridBagConstraints.BOTH;
         panel.add(label2, c);
 
-        label3.setFont(new Font("Dialog", Font.PLAIN, 36));
+        label3.setFont(new Font("Arial", Font.PLAIN, 36));
         label3.setText(String.valueOf(stats.getWinPercent())); //win%
+        label3.setForeground(new Color(255, 255, 255));
         label3.setHorizontalAlignment(SwingConstants.CENTER);
         c = new GridBagConstraints();
         c.gridx = 1;
@@ -80,8 +84,9 @@ public class displayStats extends JFrame {
         c.fill = GridBagConstraints.BOTH;
         panel.add(label3, c);
 
-        label4.setFont(new Font("Dialog", Font.PLAIN, 36));
+        label4.setFont(new Font("Arial", Font.PLAIN, 36));
         label4.setText(String.valueOf(stats.getCurrentStreak())); //current streak
+        label4.setForeground(new Color(255, 255, 255));
         label4.setHorizontalAlignment(SwingConstants.CENTER);
         c = new GridBagConstraints();
         c.gridx = 2;
@@ -89,8 +94,9 @@ public class displayStats extends JFrame {
         c.fill = GridBagConstraints.BOTH;
         panel.add(label4, c);
 
-        label5.setFont(new Font("Dialog", Font.PLAIN, 36));
+        label5.setFont(new Font("Arial", Font.PLAIN, 36));
         label5.setText(String.valueOf(stats.getMaxStreak())); //max streak
+        label5.setForeground(new Color(255, 255, 255));
         label5.setHorizontalAlignment(SwingConstants.CENTER);
         c = new GridBagConstraints();
         c.gridx = 3;
@@ -98,7 +104,8 @@ public class displayStats extends JFrame {
         c.fill = GridBagConstraints.BOTH;
         panel.add(label5, c);
 
-        label6.setFont(new Font("Dialog", Font.PLAIN, 12));
+        label6.setFont(new Font("Arial", Font.PLAIN, 12));
+        label6.setForeground(new Color(255, 255, 255));
         label6.setText("Played");
         label6.setHorizontalAlignment(SwingConstants.CENTER);
         c = new GridBagConstraints();
@@ -107,8 +114,9 @@ public class displayStats extends JFrame {
         c.fill = GridBagConstraints.BOTH;
         panel.add(label6, c);
 
-        label7.setFont(new Font("Dialog", Font.PLAIN, 12));
+        label7.setFont(new Font("Arial", Font.PLAIN, 12));
         label7.setText("Win %");
+        label7.setForeground(new Color(255, 255, 255));
         label7.setHorizontalAlignment(SwingConstants.CENTER);
         c = new GridBagConstraints();
         c.gridx = 1;
@@ -116,8 +124,9 @@ public class displayStats extends JFrame {
         c.fill = GridBagConstraints.BOTH;
         panel.add(label7, c);
 
-        label8.setFont(new Font("Dialog", Font.PLAIN, 12));
+        label8.setFont(new Font("Arial", Font.PLAIN, 12));
         label8.setText("Current Streak");
+        label8.setForeground(new Color(255, 255, 255));
         label8.setHorizontalAlignment(SwingConstants.CENTER);
         c = new GridBagConstraints();
         c.gridx = 2;
@@ -125,8 +134,9 @@ public class displayStats extends JFrame {
         c.fill = GridBagConstraints.BOTH;
         panel.add(label8, c);
 
-        label9.setFont(new Font("Dialog", Font.PLAIN, 12));
+        label9.setFont(new Font("Arial", Font.PLAIN, 12));
         label9.setText("Max Streak");
+        label9.setForeground(new Color(255, 255, 255));
         label9.setHorizontalAlignment(SwingConstants.CENTER);
         c = new GridBagConstraints();
         c.gridx = 3;
@@ -139,11 +149,13 @@ public class displayStats extends JFrame {
         panel = new JPanel();
         panel.setPreferredSize(new Dimension(120, 150));
         panelLayout = new GridBagLayout();
+        panel.setBackground(new Color(18, 18, 19));
         panelLayout.columnWidths = new int[]{60};
         panel.setLayout(panelLayout);
 
         label = new JLabel("GUESS DISTRIBUTION");
         label.setFont(new Font("Arial", Font.BOLD, 16));
+        label.setForeground(new Color(255, 255, 255));
         label.setHorizontalAlignment(SwingConstants.CENTER);
         c = new GridBagConstraints();
         c.gridx = 0;
@@ -154,8 +166,10 @@ public class displayStats extends JFrame {
         panel.add(label, c);
 
         label = new JLabel("1");
+        label.setForeground(new Color(255, 255, 255));
         label.setFont(new Font("Arial", Font.PLAIN, 14));
         label.setHorizontalAlignment(SwingConstants.RIGHT);
+        label.setForeground(new Color(255, 255, 255));
         label.setHorizontalTextPosition(SwingConstants.CENTER);
         c = new GridBagConstraints();
         c.gridx = 0;
@@ -167,9 +181,11 @@ public class displayStats extends JFrame {
         bar = new JProgressBar(0, mostOften);
         bar.setValue(stats.getRounds().get(0));
         bar.setStringPainted(true);
+        bar.setBorderPainted(false);
         bar.setString(String.valueOf(stats.getRounds().get(0)));
+        bar.setBackground(new Color(18, 18, 19));
         bar.setFont(new Font("Arial", Font.PLAIN, 14));
-        bar.setMinimumSize(new Dimension(250, 20));
+        bar.setForeground(new Color(83, 141, 78));        bar.setMinimumSize(new Dimension(250, 20));
         bar.setPreferredSize(new Dimension(250, 20));
         c = new GridBagConstraints();
         c.gridx = 1;
@@ -179,6 +195,7 @@ public class displayStats extends JFrame {
         panel.add(bar, c);
 
         label = new JLabel("2");
+        label.setForeground(new Color(255, 255, 255));
         label.setFont(new Font("Arial", Font.PLAIN, 14));
         label.setHorizontalAlignment(SwingConstants.RIGHT);
         label.setHorizontalTextPosition(SwingConstants.CENTER);
@@ -192,9 +209,11 @@ public class displayStats extends JFrame {
         bar = new JProgressBar(0, mostOften);
         bar.setValue(stats.getRounds().get(1));
         bar.setStringPainted(true);
+        bar.setBorderPainted(false);
         bar.setString(String.valueOf(stats.getRounds().get(1)));
+        bar.setBackground(new Color(18, 18, 19));
         bar.setFont(new Font("Arial", Font.PLAIN, 14));
-        bar.setMinimumSize(new Dimension(250, 20));
+        bar.setForeground(new Color(83, 141, 78));        bar.setMinimumSize(new Dimension(250, 20));
         bar.setPreferredSize(new Dimension(250, 20));
         c = new GridBagConstraints();
         c.gridx = 1;
@@ -203,6 +222,7 @@ public class displayStats extends JFrame {
         panel.add(bar, c);
 
         label = new JLabel("3");
+        label.setForeground(new Color(255, 255, 255));
         label.setFont(new Font("Arial", Font.PLAIN, 14));
         label.setHorizontalAlignment(SwingConstants.RIGHT);
         label.setHorizontalTextPosition(SwingConstants.CENTER);
@@ -216,9 +236,11 @@ public class displayStats extends JFrame {
         bar = new JProgressBar(0, mostOften);
         bar.setValue(stats.getRounds().get(2));
         bar.setStringPainted(true);
+        bar.setBorderPainted(false);
         bar.setString(String.valueOf(stats.getRounds().get(2)));
+        bar.setBackground(new Color(18, 18, 19));
         bar.setFont(new Font("Arial", Font.PLAIN, 14));
-        bar.setMinimumSize(new Dimension(250, 20));
+        bar.setForeground(new Color(83, 141, 78));        bar.setMinimumSize(new Dimension(250, 20));
         bar.setPreferredSize(new Dimension(250, 20));
         c = new GridBagConstraints();
         c.gridx = 1;
@@ -227,6 +249,7 @@ public class displayStats extends JFrame {
         panel.add(bar, c);
 
         label = new JLabel("4");
+        label.setForeground(new Color(255, 255, 255));
         label.setFont(new Font("Arial", Font.PLAIN, 14));
         label.setHorizontalAlignment(SwingConstants.RIGHT);
         label.setHorizontalTextPosition(SwingConstants.CENTER);
@@ -240,9 +263,11 @@ public class displayStats extends JFrame {
         bar = new JProgressBar(0, mostOften);
         bar.setValue(stats.getRounds().get(3));
         bar.setStringPainted(true);
+        bar.setBorderPainted(false);
         bar.setString(String.valueOf(stats.getRounds().get(3)));
+        bar.setBackground(new Color(18, 18, 19));
         bar.setFont(new Font("Arial", Font.PLAIN, 14));
-        bar.setMinimumSize(new Dimension(250, 20));
+        bar.setForeground(new Color(83, 141, 78));        bar.setMinimumSize(new Dimension(250, 20));
         bar.setPreferredSize(new Dimension(250, 20));
         c = new GridBagConstraints();
         c.gridx = 1;
@@ -251,6 +276,7 @@ public class displayStats extends JFrame {
         panel.add(bar, c);
 
         label = new JLabel("5");
+        label.setForeground(new Color(255, 255, 255));
         label.setFont(new Font("Arial", Font.PLAIN, 14));
         label.setHorizontalAlignment(SwingConstants.RIGHT);
         label.setHorizontalTextPosition(SwingConstants.CENTER);
@@ -264,9 +290,11 @@ public class displayStats extends JFrame {
         bar = new JProgressBar(0, mostOften);
         bar.setValue(stats.getRounds().get(4));
         bar.setStringPainted(true);
+        bar.setBorderPainted(false);
         bar.setString(String.valueOf(stats.getRounds().get(4)));
+        bar.setBackground(new Color(18, 18, 19));
         bar.setFont(new Font("Arial", Font.PLAIN, 14));
-        bar.setMinimumSize(new Dimension(250, 20));
+        bar.setForeground(new Color(83, 141, 78));        bar.setMinimumSize(new Dimension(250, 20));
         bar.setPreferredSize(new Dimension(250, 20));
         c = new GridBagConstraints();
         c.gridx = 1;
@@ -275,6 +303,7 @@ public class displayStats extends JFrame {
         panel.add(bar, c);
 
         label = new JLabel("6");
+        label.setForeground(new Color(255, 255, 255));
         label.setFont(new Font("Arial", Font.PLAIN, 14));
         label.setHorizontalAlignment(SwingConstants.RIGHT);
         label.setHorizontalTextPosition(SwingConstants.CENTER);
@@ -286,11 +315,13 @@ public class displayStats extends JFrame {
         panel.add(label, c);
 
         bar = new JProgressBar(0, mostOften);
+        bar.setBorderPainted(false);
         bar.setValue(stats.getRounds().get(5));
         bar.setStringPainted(true);
         bar.setString(String.valueOf(stats.getRounds().get(5)));
+        bar.setBackground(new Color(18, 18, 19));
         bar.setFont(new Font("Arial", Font.PLAIN, 14));
-        bar.setMinimumSize(new Dimension(250, 20));
+        bar.setForeground(new Color(83, 141, 78));        bar.setMinimumSize(new Dimension(250, 20));
         bar.setPreferredSize(new Dimension(250, 20));
         c = new GridBagConstraints();
         c.gridx = 1;
@@ -301,11 +332,15 @@ public class displayStats extends JFrame {
         frame.add(panel);
 
         panel = new JPanel();
+        panel.setBackground(new Color(18, 18, 19));
         panel.setPreferredSize(new Dimension(120, 150));
         panel.setLayout(new GridBagLayout());
 
         button1 = new JButton("PLAY AGAIN");
+        button1.setBorderPainted(false);
         button1.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+        button1.setBackground(new Color(83, 141, 78));
+        button1.setForeground(new Color(255, 255, 255));
         button1.setFont(new Font("Arial", Font.BOLD, 20));
         button1.setPreferredSize(new Dimension(151, 52));
         c = new GridBagConstraints();
@@ -319,7 +354,10 @@ public class displayStats extends JFrame {
 
 
         button2 = new JButton("Reset Stats");
-        button2.setFont(new Font("Dialog", Font.BOLD, 14));
+        button2.setBorderPainted(false);
+        button2.setBackground(new Color(58, 58, 60));
+        button2.setForeground(new Color(255, 255, 255));
+        button2.setFont(new Font("Arial", Font.BOLD, 14));
         button2.setPreferredSize(new Dimension(150, 25));
         c = new GridBagConstraints();
         c.gridx = 0;
@@ -331,7 +369,10 @@ public class displayStats extends JFrame {
         button2.addActionListener(new ButtonListener(stats, frame));
 
         button3 = new JButton("Quit");
-        button3.setFont(new Font("Dialog", Font.BOLD, 14));
+        button3.setBorderPainted(false);
+        button3.setBackground(new Color(58, 58, 60));
+        button3.setForeground(new Color(255, 255, 255));
+        button3.setFont(new Font("Arial", Font.BOLD, 14));
         button3.setPreferredSize(new Dimension(150, 25));
         c = new GridBagConstraints();
         c.gridx = 1;
